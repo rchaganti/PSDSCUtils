0d2d4a6ffdb663b9a40e54423fa89e4df85f2ab2
Ravikanth C <ravikanth@ravichaganti.com>
2017-08-14 14:32:13 +0530

    Updated scripts

---
+++ Get-DscResourceMetaData.ps1
@@ -0,0 +1,39 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $Name,
+
+    [Parameter()]
+    [String]
+    $Module
+)
+
+if ($Name -or $Module)
+{
+    $resourceInfo = Get-DscResource @PSBoundParameters    
+    foreach ($resource in $resourceInfo)
+    {
+        $resourceBag = [Ordered] @{}
+        $resourcePropertyInfo = $resource.Properties
+        $propertyBag = [ordered] @{}
+        foreach ($property in $resourcePropertyInfo)
+        {
+            $propertyHash = [Ordered] @{
+                'Type'        = $property.PropertyType
+                'IsMandatory' = $property.IsMandatory
+                'Values'      = $property.Values 
+            }
+            $propertyBag.Add($Property.Name, $propertyHash)
+        }
+
+        $resourceBag.Add($resource.Name, $propertyBag)
+        $resourceBag
+    }
+
+}
+else
+{
+    throw 'Must provide either Name or ModuleName or both'
+}
+
