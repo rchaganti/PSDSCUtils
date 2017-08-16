4905dd03e631b8408ec6ca004d30ba9a1ff54563
Ravikanth C <ravikanth@ravichaganti.com>
2017-08-16 13:36:18 +0530

    updates - 816

--- Get-DscResourceProperty.ps1
+++ Get-DscResourceProperty.ps1
@@ -13,13 +13,17 @@ Process
 {
     [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::ClearCache() 
     $functionsToDefine = New-Object -TypeName 'System.Collections.Generic.Dictionary[string,ScriptBlock]'([System.StringComparer]::OrdinalIgnoreCase)
-    
+    $builtinModules = @('PSDesiredStateConfiguration'.'PSDesiredStateConfigurationEngine')
+        
     [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::LoadDefaultCimKeywords($functionsToDefine)
-    $modInfo = Get-Module -Name $ModuleName -ListAvailable
-    $schemaFilePath = $null
-    $keywordErrors = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Exception]'
-    $foundCimSchema = [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::ImportCimKeywordsFromModule($modInfo, $ResourceName, [ref] $SchemaFilePath, $functionsToDefine, $keywordErrors)
-    $foundScriptSchema = [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::ImportScriptKeywordsFromModule($modInfo, $ResourceName, [ref] $SchemaFilePath, $functionsToDefine )                
+    if ($builtinModules -notcontains $ModuleName)
+    {
+        $modInfo = Get-Module -Name $ModuleName -ListAvailable
+        $schemaFilePath = $null
+        $keywordErrors = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Exception]'
+        $foundCimSchema = [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::ImportCimKeywordsFromModule($modInfo, $ResourceName, [ref] $SchemaFilePath, $functionsToDefine, $keywordErrors)
+        $foundScriptSchema = [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::ImportScriptKeywordsFromModule($modInfo, $ResourceName, [ref] $SchemaFilePath, $functionsToDefine)
+    }
 
     $resourceProperties = ([System.Management.Automation.Language.DynamicKeyword]::GetKeyword($ResourceName)).Properties
     foreach ($key in $resourceProperties.Keys)
